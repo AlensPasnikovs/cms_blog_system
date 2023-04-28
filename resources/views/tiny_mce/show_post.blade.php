33b6b04e7c0f7ed72149f84bbc54565e374cf5ac
Alens Pasnikovs <alens.pasnikovs@gmail.com>
2023-04-28 20:18:38 +0300

    Fix iframe heigh in posts, delete profile bug fix.

--- resources/views/tiny_mce/show_post.blade.php
+++ resources/views/tiny_mce/show_post.blade.php
@@ -1,41 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-      <div class="row">
-         <div class="col-md-12">
-            <div class="card">
-               <div class="card-header">
-                  <h3 class="mt-2">{{ $post->title }}</h3>
-               </div>
-               <div class="card-body post-content hyphens">
-                  {!! $post->content !!}
-               </div>
-            </div>
+<div class="row">
+   <div class="col-md-12">
+      <div class="card">
+         <div class="card-header">
+            <h3 class="mt-2">{{ $post->title }}</h3>
+         </div>
+         <div class="card-body post-content hyphens">
+            {!! $post->content !!}
          </div>
       </div>
    </div>
+</div>
+</div>
 @endsection
 
 @section('scripts')
 
 <script>
-   document.addEventListener('DOMContentLoaded', function () {
-     var iframes = document.getElementsByTagName('iframe');
-     for (var i = 0; i < iframes.length; i++) {
-       var width = iframes[i].width;
-       iframes[i].outerHTML = '<div class="video-container" style="width: ' + width + 'px; ">' + '<div class="ratio ratio-16x9">' + iframes[i].outerHTML + '</div>' + '</div>';
-     }
-   });
+   function resizeIframes() {
+      var iframes = document.querySelectorAll('.card-body iframe');
+      iframes.forEach(function(iframe) {
+      var width = iframe.offsetWidth;
+      var height = width * 9 / 16;
+      iframe.style.height = height + 'px';
+      });
+   }
 
-   $(window).on("popstate", function (event) {
-        // reload the page content here
-        console.log("popstate");
-        $.get("getPosts", function (response) {
-            // update the HTML with the new content
-            $("#post_container").empty();
-            $("#post_container").append(response.html);
-        });
-    });
+window.addEventListener('resize', resizeIframes);
+resizeIframes();
 </script>
 
 
